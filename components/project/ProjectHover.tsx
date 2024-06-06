1e13e34a462c28859dab2431aa595746df75a8b4
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2024-06-06 13:30:01 +0400

    done projects page and start dynamic nested route

--- components/project/ProjectHover.tsx
+++ components/project/ProjectHover.tsx
@@ -2,19 +2,23 @@
 import { useEffect, useRef, useState } from "react";
 import { ArrowIcon } from "../icons/Icons";
 import gsap from "gsap";
-import me from "../../assets/me.png";
 import { useGSAP } from "@gsap/react";
+import projectsData from "@/data/projectsData";
+import Link from "next/link";
 
 const ProjectHover = () => {
   const component = useRef(null);
   const [currentItem, setCurrentItem] = useState<null | number>(null);
-  const revealRef = useRef(null);
+  const revealRef = useRef<HTMLDivElement>(null);
   const textRef = useRef(null);
   const lastMousePos = useRef({ x: 0, y: 0 });
 
-  const onMouseEnter = (index: number) => {
+  const onMouseEnter = (index: number, imageUrl: string) => {
     setCurrentItem(index);
     gsap.to(revealRef.current, { opacity: 1, duration: 0.5 });
+    if (revealRef.current) {
+      revealRef.current.style.backgroundImage = `url(${imageUrl})`;
+    }
   };
 
   const onMouseLeave = () => {
@@ -24,7 +28,7 @@ const ProjectHover = () => {
 
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
-      const mousePos = { x: e.clientX, y: e.clientY - window.scrollY };
+      const mousePos = { x: e.clientX, y: e.clientY };
       const speed = Math.sqrt(Math.pow(mousePos.x - lastMousePos.current.x, 2));
 
       if (currentItem !== null) {
@@ -65,25 +69,33 @@ const ProjectHover = () => {
 
   return (
     <section ref={component}>
-      <div
-        ref={textRef}
-        className="border-y border-y-slate-100 py-10 flex items-center justify-between cursor-pointer"
-        onMouseEnter={() => onMouseEnter(1)}
-        onMouseLeave={onMouseLeave}
-      >
-        <div>
-          <h2 className="text-3xl font-bold">Next.js Todo App</h2>
-          <h3 className="text-yellow-400">Next.js</h3>
-        </div>
-        <button className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
-          <span>View Project</span>
-          <ArrowIcon />
-        </button>
-      </div>
+      <ul>
+        {projectsData.map((project, index) => (
+          <li
+            key={project.title}
+            ref={textRef}
+            onMouseEnter={() => onMouseEnter(index, project.image)}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link
+              className="border-y border-y-slate-100 py-10 flex items-center justify-between cursor-pointer"
+              href={`/projects/${project.title.split(" ").join("-")}`}
+            >
+              <div>
+                <h2 className="text-3xl font-bold">{project.title}</h2>
+                <h3 className="text-yellow-400">{project.tech}</h3>
+              </div>
+              <button className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
+                <span>View Project</span>
+                <ArrowIcon />
+              </button>
+            </Link>
+          </li>
+        ))}
+      </ul>
       <div
         ref={revealRef}
         className="pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-cover bg-center opacity-0 transition-[background] duration-300"
-        style={{ backgroundImage: `url(${me.src})` }}
       ></div>
     </section>
   );
---
+++ data/projectsData.ts
@@ -0,0 +1,35 @@
+import Audiophile from "../assets/audio.webp";
+import LinkShare from "../assets/link-share.webp";
+import Graphql from "../assets/graphql.webp";
+import Todo from "../assets/todo.webp";
+import Chat from "../assets/chat.webp";
+
+const projectsData = [
+  {
+    title: "Audiophile Ecommerce Website",
+    tech: "React",
+    image: Audiophile.src,
+  },
+  {
+    title: "Link Sharing App",
+    tech: "React / Firebase",
+    image: LinkShare.src,
+  },
+  {
+    title: "Graphql React App",
+    tech: "React / Graphql",
+    image: Graphql.src,
+  },
+  {
+    title: "Next.js Todo App",
+    tech: "Next.js",
+    image: Todo.src,
+  },
+  {
+    title: "Chat app",
+    tech: "React / Socket.io",
+    image: Chat.src,
+  },
+];
+
+export default projectsData;
