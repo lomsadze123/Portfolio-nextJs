f07cd8edabe417ae9f743b4a268b6fef1450c3b5
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2024-06-07 18:59:59 +0400

    little change for responsive

--- components/project/CurrentProject.tsx
+++ components/project/CurrentProject.tsx
@@ -1,11 +1,10 @@
 import { Project } from "@/types/types";
-import React from "react";
 
 const CurrentProject = ({ project }: { project: Project }) => {
   return (
     <main className="max-w-[1250px] mx-auto pt-10 px-6 md:px-0 md:pt-40 text-slate-300 flex flex-col gap-24">
       <div className="bg-slate-900 px-4 py-10 md:px-8 md:py-20 text-lg rounded-2xl border-2 border-slate-800">
-        <h1 className="font-bold leading-tight tracking-tight text-slate-300 text-6xl md:text-8xl">
+        <h1 className="font-bold leading-tight tracking-tight text-slate-300 text-5xl md:text-8xl">
           {project.title}
         </h1>
         <p className="text-xl font-bold text-yellow-400">
@@ -19,7 +18,7 @@ const CurrentProject = ({ project }: { project: Project }) => {
 
         {project.sections?.map((section, index) => (
           <div key={index}>
-            <h2 className="mt-8 text-5xl text-white font-bold">
+            <h2 className="mt-8 text-3xl md:text-5xl text-white font-bold">
               {section.heading}
             </h2>
             <p className="mt-4">{section.content}</p>
@@ -31,14 +30,13 @@ const CurrentProject = ({ project }: { project: Project }) => {
               </ul>
             )}
             {section.codeBlock && (
-              <pre className="bg-gray-800 text-gray-300 p-4 rounded">
+              <pre className="bg-gray-800 text-gray-300 p-4 rounded overflow-hidden text-ellipsis whitespace-nowrap">
                 <code>{section.codeBlock}</code>
               </pre>
             )}
           </div>
         ))}
 
-        {/* Render gettingStarted section if it exists */}
         {project.gettingStarted && (
           <div>
             <h2 className="mt-8 text-5xl text-white font-bold">
@@ -48,7 +46,7 @@ const CurrentProject = ({ project }: { project: Project }) => {
               <div key={index}>
                 <p className="mt-2">{step.content}</p>
                 {step.codeBlock && (
-                  <pre className="bg-gray-800 text-gray-300 p-4 rounded">
+                  <pre className="bg-gray-800 text-gray-300 p-4 rounded overflow-hidden text-ellipsis whitespace-nowrap">
                     <code>{step.codeBlock}</code>
                   </pre>
                 )}
--- components/project/ProjectHover.tsx
+++ components/project/ProjectHover.tsx
@@ -82,7 +82,9 @@ const ProjectHover = () => {
               href={`/projects/${project.title.split(" ").join("-")}`}
             >
               <div>
-                <h2 className="text-3xl font-bold">{project.title}</h2>
+                <h2 className="text-xl md:text-3xl font-bold">
+                  {project.title}
+                </h2>
                 <h3 className="text-yellow-400">{project.tech}</h3>
               </div>
               <button className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
@@ -95,7 +97,7 @@ const ProjectHover = () => {
       </ul>
       <div
         ref={revealRef}
-        className="pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-cover bg-center opacity-0 transition-[background] duration-300"
+        className="hidden md:block pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-cover bg-center opacity-0 transition-[background] duration-300"
       ></div>
     </section>
   );
